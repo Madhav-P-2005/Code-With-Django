d01aedd355514bb5fd54f41a0be462e97f54a61e
Madhav P <madhavp2023@gmail.com>
2025-06-25 13:09:18 +0530

    Learning Phase-6 Completed

--- myapp/views.py
+++ myapp/views.py
@@ -115,7 +115,7 @@ def get_items(request):
     # Create a new SQLite connection for each request
     connection = sqlite3.connect('db.sqlite3')
     items = []
-    
+
     try:
 
         # Retrive items from the database
@@ -128,28 +128,3 @@ def get_items(request):
         connection.close()
 
     return HttpResponse(items)
-
-
-
-'''
-
-⭐)  Defining and Exporting Django Models
-
-# Save and Retrieve Data 
-
-'''
-
-from django.http import HttpResponse
-
-from .models import item
-
-def add_and_get_items(request):
-
-    item = Item(name='Item 1')
-
-    item.save()   # Save item to database
-
-    items = Item.objects.all()    # Retrieve all items   (Returns the query set )
-
-    return HttpResponse(items)
-
